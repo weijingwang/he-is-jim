0054db8dc2425b511933afc1501648712a6bc7c6
Weijing Wang <weijingrocks@gmail.com>
2018-04-15 16:30:46 -0700

    Revert "screw up"
    This reverts commit 05c91fdc050919315af601ec3e30bfa7dda798d2.

--- displayText.py
+++ displayText.py
@@ -7,19 +7,17 @@ def messageText(text,x,y,size,surface,red,green,blue,chooseFont):
 	# fontPath = os.path.dirname(os.path.abspath("Roboto-Regular.ttf"))
 	# print (fontPath)
 
-	
-	myFont = pygame.font.Font(chooseFont, size)
+	if (chooseFont == "ComicSans"):
+		myFont = pygame.font.Font("assets/fonts/ComicSansMSRegular.ttf", size)
+	elif (chooseFont == "Roboto"):
+		myFont = pygame.font.Font("assets/fonts/Roboto-Regular.ttf", size)
 
 
 	label = myFont.render(text, 1, (red, green, blue))
-	    
+
 	surface.blit(label, (x, y))
-	        
+
 	pygame.display.flip()
- 
+
  #how to use
- #messageText("bob ate a dog",x,y,size,what surface,redlevel,greenlevel,bluelevel)
-
-
-
-
+ #messageText("Text to display",x,y,size,what surface,redlevel,greenlevel,bluelevel)
