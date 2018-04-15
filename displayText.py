218152f21ebaf5ab8804a32abeb9c3b94ac912b7
Weijing Wang <weijingrocks@gmail.com>
2018-04-15 14:29:05 -0700

    display text font

--- displayText.py
+++ displayText.py
@@ -1,20 +1,24 @@
 import pygame
 import os
 pygame.init()
-def messageText(text,x,y,size,surface,red,green,blue):
+def messageText(text,x,y,size,surface,red,green,blue,font):
 	# os.getcwd()
 	# os.chdir("roboto")
 	# fontPath = os.path.dirname(os.path.abspath("Roboto-Regular.ttf"))
 	# print (fontPath)
 
-	myFont = pygame.font.Font("assets/fonts/ComicSansMSRegular.ttf", size)
+	myFont = pygame.font.Font(font, size)
 
 
 	label = myFont.render(text, 1, (red, green, blue))
-
+	    
 	surface.blit(label, (x, y))
-
+	        
 	pygame.display.flip()
-
+ 
  #how to use
- #messageText("bob ate a dog",x,y,size,what surface,redcolor,greencolor,bluecolor)
+ #messageText("bob ate a dog",x,y,size,what surface,redlevel,greenlevel,bluelevel)
+
+
+
+
