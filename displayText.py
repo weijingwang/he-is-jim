3842604a0cd38afe3bcda016effd4f4a1202c6cd
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-04-14 22:12:59 -0700

    change information in module to color instead of level

--- displayText.py
+++ displayText.py
@@ -17,4 +17,4 @@ def messageText(text,x,y,size,surface,red,green,blue):
 	pygame.display.flip()
 
  #how to use
- #messageText("bob ate a dog",x,y,size,what surface,redlevel,greenlevel,bluelevel)
+ #messageText("bob ate a dog",x,y,size,what surface,redcolor,greencolor,bluecolor)
