a139181711f7e8935e91e3c19f35e2e1916e4fc6
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-04-15 16:40:27 -0700

    make displayTxt cleaner and more efficient

--- displayText.py
+++ displayText.py
@@ -8,10 +8,12 @@ def messageText(text,x,y,size,surface,red,green,blue,chooseFont):
 	# print (fontPath)qeghry'hteihjegrwrnhpvfh07ghwruhte
 
 	if (chooseFont == "ComicSans"):
-		myFont = pygame.font.Font("assets/fonts/ComicSansMSRegular.ttf", size)
-	elif (chooseFont == "Roboto"):
-		myFont = pygame.font.Font("assets/fonts/Roboto-Regular.ttf", size)
+		fontFile = "assets/fonts/ComicSansMSRegular.ttf"
 
+	elif (chooseFont == "Roboto"):
+		fontFile = "assets/fonts/Roboto-Regular.ttf"
+
+		myFont = pygame.font.Font(fontFile, size)
 
 	label = myFont.render(text, 1, (red, green, blue))
 
