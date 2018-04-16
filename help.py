79ff5bc9ecebb78e5cc354cdbf1864b6cdbe2ca1
Weijing Wang <weijingrocks@gmail.com>
2018-04-15 18:54:14 -0700

    screww up 2

--- help.py
+++ help.py
@@ -6,15 +6,14 @@ from buttons import *
 pygame.mixer.pre_init()
 pygame.init()
 pygame.font.init()
-#screen
-x = 800
-y = 600
+
+
 black = ((0,0,0))
-screen = pygame.display.set_mode((x,y))
-#font
+
+
 
 #function for menu which you can call
-def showCredits():
+def helps(surface):
 	done = False
 	while not done:
 		for event in pygame.event.get():
@@ -23,13 +22,8 @@ def showCredits():
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_RETURN:
 					done = True
-		screen.fill(black)
-		messageText("Help",325,30,40,screen,255,255,255,"ComicSans")
-		messageText("Press the Button on the rock to kill it",100,90,20,screen,255,255,255,"ComicSans")
-		messageText("sometimes, there will be multiple letters. ",100,120,20,screen,255,255,255,"ComicSans")
-		messageText("That means you have to press them all at once.",100,150,20,screen,255,255,255,"ComicSans")
-		messageText("There may be a boss somewhere in the game.",100,180,20,screen,255,255,255,"ComicSans")
-		messageText("It may not die even after entering lots of keys!",100,210,20,screen,255,255,255,"ComicSans")
-		button("Main Menu",320,400,150,50,True,screen)
+		surface.fill(black)
+		messageText("Help",325,30,40,surface,244,67,54,"ComicSans")
+		messageText("",100,30,30,surface,244,67,54,"ComicSans")
+		button("Main Menu",320,400,150,50,True,surface)
 		pygame.display.update()
-showCredits()
