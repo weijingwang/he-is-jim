c77f8ee406d74bd524148c762f8d552c7babe2f3
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-04-16 17:28:22 -0700

    add makeDogePhrase module

--- makeDogePhrase.py
+++ makeDogePhrase.py
@@ -1,1 +1,24 @@
-firstWord = ["much","many","so","such","very"]
+import os
+import random
+
+suffix = ["","!","!!","","!!!"]
+dogeWords = ["DOGE WORDS GO HERE!"]
+
+def dogeSay():
+    firstDoge = random.randint(0, 40)
+    if (firstDoge == 1):
+        dogeWord = "wow"
+    elif (firstDoge == "2"):
+        dogeWord = "pls"
+    else:
+        dogeWord = dogeWords[random.randint(0, len(dogeWords))]
+
+    print("dogeWord set to: " + dogeWord)
+
+    print("generating suffix...")
+    dogeSuffix = suffix[random.randint(0, len(suffix))]
+    print("DogeSuffix set to: " + dogeSuffix)
+    finalDogePhrase = dogeWord + dogeSuffix
+    print("finalDogePhrase created: " + finalDogePhrase)
+
+    return finalDogePhrase
