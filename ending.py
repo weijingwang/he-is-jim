ab042242d2afc2dae7907d6fec20fd2919d2c27b
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-04-18 22:49:22 -0700

    Inital 3 layers setup for end sequence

---
+++ ending.py
@@ -0,0 +1,19 @@
+import os
+import pygame
+from displayText import *
+pygame.mixer.pre_init()
+pygame.init()
+pygame.font.init()
+#screen
+x = 800
+y = 600
+black = ((0,0,0))
+screen = pygame.display.set_mode((x,y))
+
+front_sprites = pygame.sprite.Group()
+middle_sprites = pygame.sprite.Group()
+background_sprites = pygame.sprite.Group()
+
+background_sprites.draw(screen)
+middle_sprites.draw(screen)
+front_sprites.draw(screen)
