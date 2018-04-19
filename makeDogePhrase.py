b43e8c05a7657528f2e1b02ab8d489a87acbda3e
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-04-18 20:48:58 -0700

    reinsert comments into function

--- makeDogePhrase.py
+++ makeDogePhrase.py
@@ -6,6 +6,10 @@ dogeWords = ["DOGE WORDS GO HERE!"]
 dogeColors = ["#ff5252","#ff4081","#e040fb","#7c4dff","#536dfe","#448aff","#40c4ff","#18ffff","#64ffda","#69f0ae","#b2ff59","#eeff41","#ffff00","#ffd740","#ffd740","#ff6e40","#ff1744","#f50057","#d500f9","#651fff","#3d5afe","#2979ff","#00b0ff","#00e5ff","#1de9b6","#00e676","#76ff03","#76ff03","#ffea00","#ffc400","#ff9100",""#ff3d00"]
 
 def hexToRgb(hex_value):
+
+    #Convert a hexadecimal color value to a 3-tuple of integers
+    #suitable for use in an ``rgb()`` triplet specifying that color.
+    
     hex_value = normalize_hex(hex_value)
     hex_value = int(hex_value[1:], 16)
     return IntegerRGB(
