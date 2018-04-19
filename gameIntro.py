f3eb95fa4c57471f46d0599eb272ee258b044f81
Weijing Wang <weijingrocks@gmail.com>
2018-04-18 18:33:54 -0700

    More scene Add

--- gameIntro.py
+++ gameIntro.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import pygame
 from displayText import * 
+from fadetoWhite import *
 pygame.mixer.pre_init()
 pygame.init()
 pygame.font.init()
@@ -13,14 +14,19 @@ screen = pygame.display.set_mode((x,y))
 #images
 jimVN = pygame.image.load("assets/images/jimVN.png")
 deskBG = pygame.image.load("assets/images/deskBG.png")
-
+computerBG = pygame.image.load("assets/images/deskBG2.png")
+#doge
 def gameIntro():
     done = False
     pictureCount = 0
     pictureNumber = 10
-    backImage = jimVN
+    backImage = deskBG
+    character1 = jimVN
     jimX = 0
     jimY = 50
+    character2 = "doge"
+    dogeX = 0
+    dogeY = 0
     sayWhat = 'i hate mondays'
     while not done:
         for event in pygame.event.get():
@@ -38,12 +44,30 @@ def gameIntro():
                 backImage = deskBG
                 jimX = 0
                 jimY = 50
+                sayWhat = 'Press Space'
+            elif pictureCount == 1:
                 sayWhat = 'This is Jim. He screwed up and now plays the game "Frogs in the Night" every day.'
-            if pictureCount == 1:
+            elif pictureCount == 2:
+                sayWhat = 'Jim is lonely and the only "friends" he has are are the internet.'
+            elif pictureCount == 3:
+                sayWhat = 'Today, there is a special event in "Frogs in the Night"'
+            elif pictureCount == 4:
+                sayWhat = ' and he plans to spend his entire day ingame.'
+            elif pictureCount ==5:
+                jimX = 500
+                sayWhat = '"Must g-g-get the b-b-rownn frog-g-g", says Jim,'
+            elif pictureCount ==6:
+                sayWhat = '"need to win the g-game..."'
+            elif pictureCount ==7:
                 sayWhat = ''
+                jimX = 1000
+                backImage = computerBG
+
+                #FitN screen
 
         screen.blit(backImage,(0,0))
-        screen.blit(jimVN,(jimX,jimY))
+        screen.blit(character1,(jimX,jimY))
+        #screen.blit(character2,(jimX,jimY))
         messageText(sayWhat,50,550,20,screen,255,255,255,"Roboto")
         pygame.display.update()
 
