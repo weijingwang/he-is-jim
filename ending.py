ecc3b22c21b994d38b8c10dd35d4c87095a35623
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-04-18 22:52:52 -0700

    add dogecoin layer

--- ending.py
+++ ending.py
@@ -11,9 +11,11 @@ black = ((0,0,0))
 screen = pygame.display.set_mode((x,y))
 
 front_sprites = pygame.sprite.Group()
+dogecoin_sprites = pygame.sprite.Group()
 middle_sprites = pygame.sprite.Group()
 background_sprites = pygame.sprite.Group()
 
 background_sprites.draw(screen)
 middle_sprites.draw(screen)
+dogecoin_sprites.sprite.Group()
 front_sprites.draw(screen)
