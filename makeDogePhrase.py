735064f7cafd75f1dbd6bc7b77475e5f1a74e320
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-04-18 20:46:04 -0700

    fix and revamp hex_to_rgb function in makeDogePhrase.py

--- makeDogePhrase.py
+++ makeDogePhrase.py
@@ -3,12 +3,21 @@ import random
 
 suffix = ["","!","!!","","!!!"]
 dogeWords = ["DOGE WORDS GO HERE!"]
-dogeColors = ["#ff5252","#ff4081","#e040fb","#7c4dff","#536dfe","#448aff","#40c4ff","#18ffff","#64ffda","#69f0ae","#b2ff59","#eeff41","#ffff00","#ffd740","#ffd740","#ff6e40","#ff1744","#f50057","#d500f9","#651fff","#3d5afe","#2979ff","#00b0ff","#00e5ff","#1de9b6","#00e676","#76ff03","#76ff03","#ffea00","#ffc400","#ff9100","#ff3d00"]
+dogeColors = ["#ff5252","#ff4081","#e040fb","#7c4dff","#536dfe","#448aff","#40c4ff","#18ffff","#64ffda","#69f0ae","#b2ff59","#eeff41","#ffff00","#ffd740","#ffd740","#ff6e40","#ff1744","#f50057","#d500f9","#651fff","#3d5afe","#2979ff","#00b0ff","#00e5ff","#1de9b6","#00e676","#76ff03","#76ff03","#ffea00","#ffc400","#ff9100",""#ff3d00"]
 
-def hex_to_rgb(hex):
-  hex = hex.lstrip('#')
-  hlen = len(hex)
-  return tuple(int(hex[i:i+hlen/3], 16) for i in range(0, hlen, hlen/3))
+def hex_to_rgb(hex_value):
+    """
+    Convert a hexadecimal color value to a 3-tuple of integers
+    suitable for use in an ``rgb()`` triplet specifying that color.
+    """
+    hex_value = normalize_hex(hex_value)
+    hex_value = int(hex_value[1:], 16)
+    return IntegerRGB(
+        hex_value >> 16,
+        hex_value >> 8 & 0xff,
+        hex_value & 0xff
+    )
+
 
 def dogeSay():
     firstDoge = random.randint(0, 40)
@@ -31,3 +40,5 @@ def dogeSay():
 
 def dogeColor():
     return hex_to_rgb(random.choice(dogeColors))
+
+dogeColor()
