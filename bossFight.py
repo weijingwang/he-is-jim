a54e5df8ac45d46472d197eff5b9b44c1c0fc477
Weijing Wang <weijingrocks@gmail.com>
2018-04-20 16:51:55 -0700

    added jumbi function

--- bossFight.py
+++ bossFight.py
@@ -14,6 +14,15 @@ black = ((0,0,0))
 screen = pygame.display.set_mode((x,y))
 #images
 gameBG3 = pygame.image.load("assets/images/background/stars3BG.png")
+bossSceneImage = pygame.image.load("assets/images/background/boss.png")
+jumbi1 = pygame.image.load("assets/images/jumbiBoss.png")
+jumbi2 = pygame.image.load("assets/images/jumbiBoss1.png")
+
+def jumbiBoss(x,y,angery,surface):
+	if angery == True:
+		surface.blit(jumbi2,(x,y))
+	else:
+		surface.blit(jumbi1,(x,y))
 
 #boss
 def bossLevel():
@@ -23,12 +32,20 @@ def bossLevel():
 			if event.type == pygame.QUIT:
 				quit()
 		screen.blit(gameBG3,(0,0))
-		#messageText("Jim",100,100,100,screen,255,255,255)
-		print("EBFKSBDS")
 		pygame.display.update()
 bossLevel()
 
+def bossScene():#short cutscene before actual boss fight
+	done = False
+	while not done:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				quit()
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+				bossLevel()
+				done = True
+		screen.blit(bossSceneImage,(0,0))
+		pygame.display.update()
 
-def boss():
-	pass
+
     
