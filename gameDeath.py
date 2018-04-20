af44f6265c2837b1ab9a1f312958494d75dd8b5d
Weijing Wang <weijingrocks@gmail.com>
2018-04-19 20:34:01 -0700

    game death is now importable function

--- gameDeath.py
+++ gameDeath.py
@@ -2,20 +2,16 @@
 import pygame
 pygame.mixer.pre_init()
 pygame.init()
-#screen
-x = 800
-y = 600
-black = ((0,0,0))
+
 #images
 deadScreen = pygame.image.load("assets/images/background/youDead.png")
 #music
 pygame.mixer.music.load("assets/music/gameOver.mp3")
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(1)
-screen = pygame.display.set_mode((x,y))
 
 #menu function
-def gameDeathScreen():
+def gameDeathScreen(surface):
 	done = False
 	while not done:
 		for event in pygame.event.get():
@@ -23,7 +19,7 @@ def gameDeathScreen():
 				quit()
 			if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
 				done = True
-		screen.blit(deadScreen,(0,0))
+		surface.blit(deadScreen,(0,0))
 
 		pygame.display.update()
-gameDeathScreen()
+
