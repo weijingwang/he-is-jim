5fe4f8629c28c80acc8dfd904b493300207ecb4d
Weijing Wang <weijingrocks@gmail.com>
2018-04-20 16:24:11 -0700

    boss level add

--- bossFight.py
+++ bossFight.py
@@ -1,5 +1,34 @@
+#!/usr/bin/python3
+import pygame
 import os
 import random
+from displayText import * #KYLER DO UR CREDITS
+from menu import *
+from credits import *
+pygame.mixer.pre_init()
+pygame.init()
+pygame.font.init()
+#screen
+x = 800
+y = 600
+black = ((0,0,0))
+screen = pygame.display.set_mode((x,y))
+#images
+gameBG3 = pygame.image.load("assets/images/background/stars3BG.png")
+
+#boss
+def bossLevel():
+	done = False
+	while not done:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				quit()
+		screen.fill(black)
+		messageText("Jim",100,100,100,screen,255,255,255)
+		pygame.display.update()
+bossLevel()
+
 
 def boss():
+	pass
     
